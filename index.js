d963decd31c482af08ff61ebccc2c37de0a7f75a
Wako Anindo <wakoanindo@gmail.com>
2024-05-16 10:34:57 +0100

    Add conditional logic to operate func that chooses an operation based on the chosen arguments

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ let number1;
 let operator;
 let number2;
 
-function operate (number1, operator, number2) {};
+function operate (number1, operator, number2) {
+    let result = null;
+
+    if (operator === "-") {
+        result = subtract(number1, number2);
+    } else if (operator === "+") {
+        result = add(number1, number2);
+    } else if (operator === "/") {
+        result = divide(number1, number2);
+    } else if (operator === "*") {
+        result = multiply(number1, number2);
+    } else {
+        return result
+    }
+
+    return result;
+};
